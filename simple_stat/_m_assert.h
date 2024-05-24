3a362a459e83f5578601594c9ebf2e1f9143247d
Егор Марков <e.markov@gt-sot.ru>
2024-05-24 08:40:15 +0300

    working on test_lib

--- simple_stat/_m_assert.h
+++ simple_stat/_m_assert.h
@@ -2,45 +2,53 @@
 #define _M_ASSERT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define STRLIM 512
+/* __convert_tostr() will cast value into 
+ * 'string' representaion for
+ * next check */
+#define __convert_tostr(x) (#x)
 
-#if !defined(_TEST_HEADER)
-#   define _TEST_HEADER 1
+#define _EXPECTED(x) (EXPECTED##x)
+#define _GOT(x) (GOT##x)
+#define _EXPECTED_GOT(x, y) (_EXPECTED(x)##_GOT(y))
 
-static inline void test_header() {
-    printf("========== FILE: %s ==========\n", __FILE__);
-}
+#define __cast_to_int32(x) ((int)x)
+#define __cast_to_int64(x) ((long int)x)
+#define __cast_to_uint64(x) ((unsigned long)x)
 
-#else
-
-static inline void test_header() { ; }
-
-#endif /* _TEST_HEADER */
-
-#define ASSERT(TYPE, RES, EXP, F_NAME, LINE) \
+#define __reflect(x, _intptr) \
     { \
-        TYPE _a = (TYPE) RES; \
-        TYPE _b = (TYPE) EXP; \
-        if (_a == _b) { \
-            printf("[LINE %d] %s: PASSED. [GOT %d, EXPECT %d]\n", LINE, F_NAME, RES, EXP); \
-        } \
-        else { \
-            printf("[LINE %d] %s: FAILED. [GOT %d, EXPECT %d]\n", LINE, F_NAME, RES, EXP); \
-        } \
+        char *_t = __convert_tostr(x); \
+        *_intptr = __get_value_type(_t); \
     }
-    
-#define ASSERT_STR(RES, EXP, FUNC_NAME, LINE) \
-    void LINE##FUNC_NAME(const char *res, const char *exp) \
+
+int __get_value_type(const char *_t);
+
+/* ASSERT_EQ check that two args are equal
+ * or will abort execution. */
+#define ASSERT_EQ(RES, EXP) \
     { \
-        for( ; *res, *exp; res++, exp++) { \
-            if (*res != *exp) { \
-                printf("[LINE %d] %s: FAILED. [GOT %d, EXPECT %d]\n", LINE, FUNC_NAME, RES, EXP); \
-                break; \
-            } \
-        printf("[LINE %d] %s: PASSED. [GOT %d, EXPECT %d]\n", LINE, F_NAME, RES, EXP); \
+        int *_t_ptr, _v = -1, success = 0; \
+        char *_test_res = _EXPECTED_GOT(EXP, RES); \
+        *_t_ptr = &_v; \
+        __reflect(EXP, _t_ptr); \
+        switch *_t_ptr { \
+            case 0: \
+                    success = __assert_eq_int32(__cast_to_int32(RES), \
+                                                __cast_to_int32(EXP)); \
+                    break; \
+            case 1: \
+                    success = __assert_eq_int64(__cast_to_int64(RES), \
+                                                __cast_to_int64(EXP)); \
+                    break; \
+            case 2: \
+                    success = __assert_eq_uint64(__cast_to_uint64(RES), \
+                                                __cast_to_uint64(EXP)); \
+                    break; \
+            default: \
+                     printf("undefined type for '%s'\n", #EXP); \
+                    abort(); \
         } \
-    } \
-    void LINE##FUNC_NAME(&res, &exp)
 
 #endif /* _M_ASSERT_H */
