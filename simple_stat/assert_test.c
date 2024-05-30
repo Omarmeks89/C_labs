41687aed7e3096fd85bcaff5c0e970a05f602d6a
Егор Марков <e.markov@gt-sot.ru>
2024-05-30 22:35:05 +0300

    added new test cases, ingnored .swp files

--- simple_stat/assert_test.c
+++ simple_stat/assert_test.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+
 #include "_m_assert.h"
+
+int test_LINE_macro_success() {
+    int exp = __LINE__, res = LINE();
+    ASSERT_EQ_INT32(res, exp, LINE());
+    return 0;
+}
 
 /* test that ASSERT_EQ_INT32 compare
  * numbers correct. */
@@ -14,13 +22,40 @@ int test_expect_eq_int32() {
     return 0;
 }
 
+int test_expect_eq_int32_failed() {
+    int exp = 10, res = 8;
+    EXPECT_EQ_INT32(res, exp, LINE());
+    return 0;
+}
+
+int test_assert_eq_int32_aborted() {
+    int exp = 10, res = 0;
+    ASSERT_EQ_INT32(res, exp, LINE());
+    printf("TEST SHOULD FAILED\n");
+    return 1;
+}
+
 int main() {
     int res = 0;
+
+    res = test_LINE_macro_success();
+    if (res != 0)
+        return res;
+
     res = test_assert_eq_int32();
     if (res != 0)
         return res;
 
     res = test_expect_eq_int32();
+    if (res != 0)
+        return res;
+
+    /* think about another strategy here. */
+    res = test_expect_eq_int32_failed();
+    if (res != 0)
+        return res;
+
+    res = test_assert_eq_int32_aborted();
     if (res != 0)
         return res;
 
