b0c8f1282aed147a69dfde5a7793a239ab4d13c7
Егор Марков <e.markov@gt-sot.ru>
2024-06-02 13:40:12 +0300

    _m_assert.h upgrade

--- simple_stat/_m_assert.h
+++ simple_stat/_m_assert.h
@@ -5,8 +5,116 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define LINE() (__LINE__)
+#define PASSED "PASSED"
+#define FAILED "FAILED"
+#define EXPECTED "EXPECTED: "
+#define GOT "GOT: "
+#define LINE_STR "LINE: "
 
+/* section of spaces and
+ * puctuation symbols. */
+#define SINGLE_SPACE " "
+#define COMMA_SPACE ", "
+#define SINGLE_POINT "."
+
+/* brackets */
+#define SQR_BRACKET_OPEN "["
+#define SQR_BRACKET_CLOSE "]"
+
+#define RND_BRACKET_OPEN "("
+#define RND_BRACKET_CLOSE ")"
+
+/* special symbols and
+ * formatting symbols */
+#define FNAME_OFFSET "%-16s"
+#define LINE_NO_FMT "%6d"
+#define TEST_STATUS_OFFSET "%32s"
+
+#ifndef INT32_FMT_TEMPL
+#  define INT32_FMT_TEMPL "%8d"
+#endif
+
+#ifndef INT64_FMT_TEMPL
+#  define INT64_FMT_TEMPL "%16ld"
+#endif
+
+#ifndef DBL_FMT_TEMPL
+#  define DBL_FMT_TEMPL "%10.6lf"
+#endif
+
+#define _T_EOL "\n"
+
+/* macro-template for passed test message 
+ * Params:
+ *   __type: format string with arg type
+ *           that used in 'printf' function.
+ *           Example:
+ *             - '%6d'
+ * Returns:
+ *   Joined literal with format directives. */
+#define _PASS_MSG(__type) (             \
+        FNAME_OFFSET                    \
+        SQR_BRACKET_OPEN                \
+        EXPECTED                        \
+        __type                          \
+        COMMA_SPACE                     \
+        GOT                             \
+        __type                          \
+        SQR_BRACKET_CLOSE               \
+        TEST_STATUS_OFFSET              \
+        SINGLE_POINT                    \
+        _T_EOL)
+
+/* macro-template for failed test message 
+ * Params:
+ *   __type: format string with arg type
+ *           that used in 'printf' function.
+ *           Example:
+ *             - '%6d'
+ * Returns:
+ *   Joined literal with format directives. */
+#define _FAIL_MSG(__type) (             \
+        FNAME_OFFSET                    \
+        RND_BRACKET_OPEN                \
+        LINE_STR                        \
+        LINE_NO_FMT                     \
+        RND_BRACKET_CLOSE               \
+        SINGLE_SPACE                    \
+        SQR_BRACKET_OPEN                \
+        EXPECTED                        \
+        __type                          \
+        COMMA_SPACE                     \
+        GOT                             \
+        __type                          \
+        SQR_BRACKET_CLOSE               \
+        TEST_STATUS_OFFSET              \
+        SINGLE_POINT                    \
+        _T_EOL)
+
+/* macro for set message about test pass.
+ * Params:
+ *   __type: format string with arg type
+ *           that used in 'printf' function.
+ *           Example:
+ *             - '%6d';
+ *   FN:     function name;
+ *   E:      Expected value;
+ *   G:      Got value. */
+#define TEST_PASSED_MSG(__type, FN, E, G) (printf(_PASS_MSG(__type), #FN, E, G, PASSED))
+
+/* macro for set message about test fail.
+ * Params:
+ *   __type: format string with arg type
+ *           that used in 'printf' function.
+ *           Example:
+ *             - '%6d';
+ *   FN:     function name;
+ *   LN:    line no for found failed test case;
+ *   E:      Expected value;
+ *   G:      Got value. */
+#define TEST_FAILED_MSG(__type, FN, LN, E, G) (printf(_FAIL_MSG(__type), #FN, LN, E, G, FAILED))
+
+/* type casting */
 #define __eq_int32(a, b) ((int) a == (int) b)
 
 #define __ne_int32(a, b) ((int) a != (int) b)
@@ -15,16 +123,19 @@
 
 #define __ne_double64(a, b, eps) ((fabs((double) a - (double) b) > (double) eps))
 
+#define LINE() (__LINE__)
+
 /* ASSERT_EQ check that two args are equal
  * or will abort execution. */
-#define ASSERT_EQ_INT32(RES, EXP, LINE) \
+#define ASSERT_EQ_INT32(RES, EXP, F_NAME, LINE) \
     { \
         int success = 0; \
         success = __eq_int32(RES, EXP); \
         if (success) { \
-            printf("PASSED. [EXPECTED: %d, GOT: %d]\n", EXP, RES); \
+            printf("%32s [EXPECTED: %d, GOT: %d] %16s.\n", #F_NAME, EXP, RES, PASSED); \
+            TEST_PASSED_MSG(INT32_FMT_TEMPL, F_NAME, EXP, RES); \
         } else { \
-            printf("(LINE: %6d) FAILED. [EXPECTED: %d, GOT: %d]\n", LINE, EXP, RES); \
+            printf("%32s (LINE: %6d) [EXPECTED: %d, GOT: %d] %16s.\n", #F_NAME, LINE, EXP, RES, FAILED); \
             abort(); \
         } \
     }
--- simple_stat/assert_test.c
+++ simple_stat/assert_test.c
@@ -4,7 +4,7 @@
 
 int test_LINE_macro_success() {
     int exp = __LINE__, res = LINE();
-    ASSERT_EQ_INT32(res, exp, LINE());
+    ASSERT_EQ_INT32(res, exp, test_LINE_macro_success, LINE());
     return 0;
 }
 
@@ -12,7 +12,7 @@ int test_LINE_macro_success() {
  * numbers correct. */
 int test_assert_eq_int32() {
     int exp = 10, res = 10;
-    ASSERT_EQ_INT32(res, exp, LINE());
+    ASSERT_EQ_INT32(res, exp, test_assert_eq_int32, LINE());
     return 0;
 }
 
@@ -30,7 +30,7 @@ int test_expect_eq_int32_failed() {
 
 int test_assert_eq_int32_aborted() {
     int exp = 10, res = 0;
-    ASSERT_EQ_INT32(res, exp, LINE());
+    ASSERT_EQ_INT32(res, exp, test_assert_eq_int32_aborted, LINE());
     printf("TEST SHOULD FAILED\n");
     return 1;
 }
